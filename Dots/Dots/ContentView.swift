1019abc177b460c827e270de4e8658d95f40ded9
Jack Zhao <jack_zhao98@outlook.com>
2021-01-08 22:11:51 -0800

    Some files added

---
+++ Dots/Dots/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dots
+//
+//  Created by Jack Zhao on 1/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
