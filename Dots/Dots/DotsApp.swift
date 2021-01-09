92b5e2a4e4aeea8d34b55e22bd5f68a887ba3e73
Jack Zhao <jack.zjzhao@gmail.com>
2021-01-08 22:15:29 -0800

    Project files added (#15)
    iOS Xcode Project file created

---
+++ Dots/Dots/DotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotsApp.swift
+//  Dots
+//
+//  Created by Jack Zhao on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct DotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
