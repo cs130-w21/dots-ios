70b1e0d150e3ac1f118ae5a68ba715d00f234d91
Jack Zhao <jack_zhao98@outlook.com>
2021-01-09 23:19:27 -0800

    conflict resolve

---
+++ Dots/Dots/BillCardView.swift
@@ -0,0 +1,20 @@
+//
+//  BillCardView.swift
+//  Dots
+//
+//  Created by Jack Zhao on 1/8/21.
+//
+
+import SwiftUI
+
+struct BillCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BillCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BillCardView()
+    }
+}
--- Dots/Dots/DotsApp.swift
+++ Dots/Dots/DotsApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct DotsApp: App {
+<<<<<<< HEAD
     @ObservedObject private var data = MainData()
     var body: some Scene {
         WindowGroup {
@@ -20,6 +21,11 @@ struct DotsApp: App {
             .onAppear {
                 data.load()
             }
+=======
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+>>>>>>> 92b5e2a4e4aeea8d34b55e22bd5f68a887ba3e73
         }
     }
 }
