c59d4e09cabffef547999fcdbadb752aab8c090a
GMA5566 <maguanqun0212@gmail.com>
2021-01-10 21:55:25 -0800

    Added a single card-like structure for a single transaction

--- Dots/Dots/SingleCard.swift
+++ Dots/Dots/SingleCard.swift
@@ -10,35 +10,44 @@ import SwiftUI
 struct SingleCard: View {
     let card: BillObject
     var body: some View {
-        List {
+        ZStack {
             VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text(card.title)
-                            if card.paid {
-                                Image(systemName: "checkmark.circle.fill")
+                    Spacer()
+                    HStack {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(card.title)
+                                if card.paid {
+                                    Image(systemName: "checkmark.circle.fill")
+                                }
+                                else {
+                                    Image(systemName: "checkmark.circle")
+                                }
                             }
-                            else {
-                                Image(systemName: "checkmark.circle")
-                            }
+                            Text("Date")
                         }
-                        Text("Date")
+                        Spacer()
+                        Text("$" + String(card.billAmount))
                     }
                     Spacer()
-                    Text("$" + String(card.billAmount))
                 }
-                HStack {
-                    ForEach(card.attendees, id: \.self) { attendees in
-                        Circle()
-                            .fill(dotColors[attendees])
-                            .frame(width: 20, height: 20)
+                .padding()
+                VStack {
+                    Spacer()
+                    HStack {
+                            ForEach(card.attendees, id: \.self) { attendees in
+                                Circle()
+                                    .fill(dotColors[attendees])
+                                    .frame(width: 20, height: 20)
+                            }
                     }
+                    .padding()
                 }
-            }
-            .padding()
         }
-        .listStyle(InsetGroupedListStyle())
+        .frame(width: 300, height: 200, alignment: .center)
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 30, x: 0, y: 10)
     }
 }
 
@@ -46,6 +55,6 @@ struct SingleCard_Previews: PreviewProvider {
     static var card = BillObject.sample[0]
     static var previews: some View {
         SingleCard(card: card)
-            .previewLayout(.fixed(width: 400, height: 60))
+            .previewLayout(.sizeThatFits)
     }
 }
