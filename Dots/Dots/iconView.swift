d0a05022950ec183d4cf23573905ceb53fcd28bf
Jack Zhao <jack.zjzhao@gmail.com>
2021-01-14 14:34:28 -0800

    Update icon view

--- Dots/Dots/iconView.swift
+++ Dots/Dots/iconView.swift
@@ -10,37 +10,11 @@ import SwiftUI
 struct iconView: View {
     var body: some View {
         ZStack {
-            Color(UIColor.systemGray6)
-            VStack(alignment: .leading, spacing: 10) {
-                Spacer()
-                HStack {
-                    Circle()
-                        .frame(width: 110, height: 110, alignment: .center)
-                    Circle()
-                        .frame(width: 110, height: 110, alignment: .center)
-                        .offset(y: 8)
-                    Circle()
-                        .frame(width: 110, height: 110, alignment: .center)
-                        .offset(x: -13, y: 60)
-                }
-                Circle()
-                    .frame(width: 110, height: 110, alignment: .center)
-                HStack {
-                    Circle()
-                        .frame(width: 110, height: 110, alignment: .center)
-                    Circle()
-                        .frame(width: 110, height: 110, alignment: .center)
-                        .offset(y: -8)
-                    Circle()
-                        .foregroundColor(.orange)
-                        .frame(width: 110, height: 110, alignment: .center)
-                        .offset(x: -13, y: -60)
-                }
-                Spacer()
-            }
-            .offset(x: 10)
-            .foregroundColor(.red)
-            .shadow(radius: 5, x:0, y:5)
+//            Color(UIColor.systemGray6)
+            LinearGradient(gradient: Gradient(colors: [Color.purple, Color.red, Color.yellow]), startPoint: .top, endPoint: .bottom)
+                .mask(DotICon())
+//                .shadow(radius: 10, x:2, y:5)
+            
            
         }
         .frame(width: 512, height: 512)
@@ -51,5 +25,40 @@ struct iconView_Previews: PreviewProvider {
     static var previews: some View {
         iconView()
             .previewLayout(.sizeThatFits)
+    }
+}
+
+struct DotICon: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Spacer()
+            HStack {
+                Circle()
+                    .frame(width: 110, height: 110, alignment: .center)
+                Circle()
+                    .frame(width: 110, height: 110, alignment: .center)
+                    .offset(y: 8)
+                Circle()
+                    .frame(width: 110, height: 110, alignment: .center)
+                    .offset(x: -13, y: 60)
+            }
+            Circle()
+                .frame(width: 110, height: 110, alignment: .center)
+            HStack {
+                Circle()
+                    .frame(width: 110, height: 110, alignment: .center)
+                Circle()
+                    .frame(width: 110, height: 110, alignment: .center)
+                    .offset(y: -8)
+                Circle()
+                    .foregroundColor(.orange)
+                    .frame(width: 110, height: 110, alignment: .center)
+                    .offset(x: -13, y: -60)
+            }
+            Spacer()
+        }
+        .offset(x: 10)
+        .foregroundColor(.red)
+        .shadow(radius: 5, x:0, y:5)
     }
 }
