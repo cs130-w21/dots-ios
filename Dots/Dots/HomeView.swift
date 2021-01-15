acb6a01a731c71693c2ef734db1b71e177029936
Jack Zhao <jack.zjzhao@gmail.com>
2021-01-14 17:47:01 -0800

    Minor update

--- Dots/Dots/HomeView.swift
+++ Dots/Dots/HomeView.swift
@@ -13,40 +13,45 @@ struct HomeView: View {
     @State var showDots: Bool = false
     var body: some View {
         ZStack {
+            
             Color(UIColor.systemGray6)
                 .ignoresSafeArea()
-            
-            
+//
+//            VStack {
+//                ForEach (0..<10) { i in
+//                Rectangle()
+//                    .foregroundColor(dotColors[i])
+//                    .frame(height: 30)
+//                }
+//            }
             ScrollView (.vertical, showsIndicators: false) {
-                
-                DotSelectView(show: $showDots, circleRadius: 110, inGroup: $groups, allDots: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-//                    .padding(.top, 60)
-                    .padding(.vertical)
-                    .zIndex(1.0)
-                
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 280), spacing: 20)], spacing: 25) {
-                    ForEach(bills) { i in
-                        CardView(card: binding(for: i))
-                            .background(Color(UIColor.systemBackground))
-                            .frame(height: 200)
-                            .cornerRadius(40)
-                            .shadow(radius: 10, x: 5, y: 5)
+                    DotSelectView(show: $showDots, circleRadius: 110, inGroup: $groups, allDots: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+                        .padding(.vertical)
+                        .zIndex(1.0)
+                        .blur(radius: 0)
+                    
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 280), spacing: 20)], spacing: 25) {
+                        ForEach(bills) { i in
+                            CardView(card: binding(for: i))
+                                .frame(height: 200)
+                                .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                                .shadow(radius: 10, x: 5, y: 5)
+                        }
                     }
-                }
-                .padding(.horizontal, 30)
-                .padding(.bottom, 100)
-                .blur(radius: showDots ? 20 : 0)
-                
+                    .blur(radius: showDots ? 10 : 0)
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 100)
             }
-            .onTapGesture {
-                if showDots {
-                    withAnimation {
-                        showDots = false
-                        haptic_one_click()
-                    }
+        }
+        .onTapGesture {
+            if showDots {
+                withAnimation {
+                    showDots = false
+//                    haptic_one_click()
                 }
             }
         }
+        
         
     }
     
@@ -61,5 +66,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(groups: .constant([1,2,3,4,5]), bills: .constant(BillObject.sample), showDots: false)
+            .previewDevice("iPhone 11")
     }
 }
