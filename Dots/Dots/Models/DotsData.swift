e5507019c43195ac0d38d5c16652b7f9b374783d
GMA5566 <maguanqun0212@gmail.com>
2021-02-04 19:55:46 -0800

    Finished DotsData's mutator, EntryObject's accessor and mutator.

--- Dots/Dots/Models/DotsData.swift
+++ Dots/Dots/Models/DotsData.swift
@@ -85,27 +85,37 @@ struct DotsData: Identifiable, Codable {
     // MARK: Mutators
     //TODO: add member to group
     mutating func addToGroup(i: Int /* 0-9 */) {
-        
+        self.group.append(i)
     }
     
     // TODO: remove a member from group
     mutating func removeFromGroup(i: Int /* 0-9 */) {
-        
+        for index in 0...self.group.count-1 {
+            if self.group[index] == i {
+                self.group.remove(at: index)
+                return
+            }
+        }
     }
     
     //TODO: Clear all bills
     mutating func clearBills() {
-        
+        self.bills = []
     }
     
     // TODO: Add new bill
     mutating func addNewBill(bill: BillObject) {
-        
+        self.bills.append(bill)
     }
     
     // TODO: Remove bill by id
     mutating func removeBillById(id: UUID) {
-        
+        for index in 0...self.bills.count-1 {
+            if self.bills[index].id == id {
+                self.bills.remove(at: index)
+                return
+            }
+        }
     }
     
 }
