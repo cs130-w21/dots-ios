a0aebd992c4cb8f6a73d0db0d9cea2cdb7ecbf8c
Jack Zhao <jack.zjzhao@gmail.com>
2021-02-24 01:39:20 -0800

    Add docs to top level

--- Dots/Dots/DotsApp.swift
+++ Dots/Dots/DotsApp.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 @main
 struct DotsApp: App {
+    
+    /// The observed master data object of the app, contains user data and user settings.
     @ObservedObject private var data = MainData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView(mainData: $data.mainData) {
